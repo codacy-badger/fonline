fbecd2864931b0f93ac7fcada3131f08e8269398
cvet <cvet@tut.by>
2019-08-08 20:19:55 +0300

    SDL2 2.0.10

--- Source/SDL2/src/dynapi/SDL_dynapi.h
+++ Source/SDL2/src/dynapi/SDL_dynapi.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2018 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2019 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -56,8 +56,6 @@
 #elif defined(__clang_analyzer__)
 #define SDL_DYNAMIC_API 0  /* Turn off for static analysis, so reports are more clear. */
 #endif
-
-#define SDL_DYNAMIC_API 0 //! FOnline
 
 /* everyone else. This is where we turn on the API if nothing forced it off. */
 #ifndef SDL_DYNAMIC_API
